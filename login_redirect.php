8528c7d763ca4719ff47526608b88b4408345f66
eesenwa <eesenwa@brandeis.edu>
2016-05-12 11:29:37 -0400

    Add files via upload

---
+++ login_redirect.php
@@ -0,0 +1,63 @@
+<html>
+<body>
+	Data submission: <br>
+	<?php  
+		$con = mysqli_connect('localhost', 'root', '') or die(mysql_error());
+		mysqli_select_db($con, 'user_registration') or die('cannot select DB');
+
+		if(!empty($_POST['s_email'])) {
+			//Student account
+			echo '<strong> Account type: </strong>: 0<br>';
+			echo '<strong> Email/phone:</strong> '. $_POST['s_email'] . '<br>';
+			echo '<strong> Password:</strong> '. $_POST['s_pass'] . '<br>';
+			echo '<strong> Group code:</strong> '. $_POST['group_code'] . '<br><br><br>';
+
+			$user = $_POST['s_email'];
+			$pass = $_POST['s_pass'];
+			$gc = $_POST['group_code'];
+
+			$query = mysqli_query($con, "SELECT * FROM login WHERE user = '".$user."'") or die(mysqli_error($con));
+			$numrows = mysqli_num_rows($query);
+			if($numrows == 0) {
+				$sql = "INSERT INTO login (userID, user, password, groupcode) VALUES ('', '$user', '$pass', '$gc')";
+				$result = mysqli_query($con, $sql);
+				if($result) {
+					echo 'Account created! <br>';
+				}else {
+					echo 'Failure to create account';
+				}
+			} else {
+				echo 'Account already exists';
+			}
+		}
+		else {
+			//Teacher account
+			echo '<strong> Account type: </strong>: 1<br>';
+			echo '<strong> First name:</strong> '. $_POST['fname'] . '<br>';
+			echo '<strong> Last name: </strong> '. $_POST['lname'] . '<br>';
+			echo '<strong> Email:</strong> '. $_POST['t_email'] . '<br>';
+			echo '<strong> Password:</strong> '. $_POST['t_pass'] . '<br>';
+
+			$fname = $_POST['fname'];
+			$pass = $_POST['lname'];
+			$t_email = $_POST['t_email'];
+			$t_password = $_POST['t_password'];
+
+			$query = mysqli_query($con, "SELECT * FROM login WHERE user = '".$user."'") or die(mysqli_error($con));
+			$numrows = mysqli_num_rows($query);
+			if($numrows == 0) {
+				$sql = "INSERT INTO login (userID, user, password, groupcode) VALUES ('', '$user', '$pass', '$gc')";
+				$result = mysqli_query($con, $sql);
+				if($result) {
+					echo 'Account created! <br>';
+				}else {
+					echo 'Failure to create account';
+				}
+			} else {
+				echo 'Account already exists';
+			}
+		}
+	?>
+
+</body>
+</html>
